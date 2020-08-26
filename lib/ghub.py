2afe69825a3ee512ecbaa57e9902158bf725f3fa
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-08-26 16:25:11 +0200

    Fix when username or repo name has '-'

--- lib/ghub.py
+++ lib/ghub.py
@@ -23,8 +23,9 @@ def get_prs_details(pr_id):
     prs = get_prs_data()
     for pr in prs:
         if pr["number"] == pr_id:
+            details = f"[{pr['user']['login']}](https://github.com/{pr['user']['login']})\n".replace("-", "\-")
             message = (f"\#{pr['number']} *{clean(pr['title'])}* by "
-                       f"[{pr['user']['login']}](https://github.com/{pr['user']['login']})\n"
+                       f"{details}"
                        f"Last update: {clean(pr['updated_at'])}\n"
                        f"{clean(pr['html_url'])}\n")
             return message
